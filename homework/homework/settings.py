8b85e6f5d83aa9433ad497ac2aed7b0c6ec814bd
dornela2 <104805972+dornela2@users.noreply.github.com>
2023-02-06 19:12:58 -0600

    Conf Database

--- homework/homework/settings.py
+++ homework/homework/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6ej%*1h8-jtb9)4wf^brnog^z+v=v9-nq82ud=c6cwjbvzbisz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'homework.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'dornela2',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
